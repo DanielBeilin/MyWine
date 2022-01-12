801f81cd5d1cdaa51715489ca3783aad4e8f86dc
PolaDinitz <44160650+PolaDinitz@users.noreply.github.com>
2022-01-12 21:26:45 +0200

    Connected profile to auth added menu for logout

--- app/src/main/java/com/example/mywine/ProfileActivity.java
+++ app/src/main/java/com/example/mywine/ProfileActivity.java
@@ -7,17 +7,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.mywine.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
+    TextView UserEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +32,15 @@ public class ProfileActivity extends AppCompatActivity {
         actionBar.setTitle("Profile");
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        UserEmail = findViewById(R.id.email);
     }
 
     private void checkUSerStatus() {
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
         if (firebaseUser != null) {
             // TODO: user is already signed in
+            UserEmail.setText(firebaseUser.getEmail());
         } else {
             startActivity(new Intent(ProfileActivity.this, SignInActivity.class));
             finish();
@@ -44,5 +51,23 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onStart() {
         checkUSerStatus();
         super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            firebaseAuth.signOut();
+            checkUSerStatus();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
