5e2963598783bc1f6bfc91c0f22dd3b3f88f2ba9
PolaDinitz <44160650+PolaDinitz@users.noreply.github.com>
2022-01-12 18:20:22 +0200

    Connected to firebase Created Login page Created Register page Created Profile page

--- app/src/main/java/com/example/mywine/RegisterActivity.java
+++ app/src/main/java/com/example/mywine/RegisterActivity.java
@@ -1,24 +1,53 @@
 package com.example.mywine;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
     Button RegisterBtn;
     TextView LoginLink;
+    EditText EmailInput, PasswordInput, NameInput;
+    ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Create Account...");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Registering User...");
+
         LoginLink = findViewById(R.id.lnkLogin);
         RegisterBtn = findViewById(R.id.btnRegister);
+        EmailInput = findViewById(R.id.txtEmail);
+        PasswordInput = findViewById(R.id.txtPwd);
+        NameInput = findViewById(R.id.txtName);
 
         LoginLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,10 +59,51 @@ public class RegisterActivity extends AppCompatActivity {
         RegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: Register logic
-                Intent intent = new Intent(RegisterActivity.this, SignInActivity.class);
-                startActivity(intent);
+                String email = EmailInput.getText().toString().trim();
+                String password = PasswordInput.getText().toString().trim();
+                String name = NameInput.getText().toString().trim();
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    EmailInput.setError("Invalid Email!");
+                    EmailInput.setFocusable(true);
+                } else if (password.length() < 6) {
+                    EmailInput.setError("Password length has to be at least 6 characters!");
+                    EmailInput.setFocusable(true);
+                } else {
+                    registerUser(email, password);
+                }
             }
         });
+    }
+
+    private void registerUser(String email, String password) {
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progressDialog.dismiss();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            progressDialog.dismiss();
+                            Toast.makeText(RegisterActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
